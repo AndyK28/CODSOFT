490846f873b9088c793ae3f257fe4483ac815d1b
akhumalo023a <akhumalo023a@student.wethinkcode.co.za>
2024-05-19 19:26:36 +0200

    Added docstrings

--- student_grade_calculator/src/main/java/student/grade/calculator/GradeCalculator.java
+++ student_grade_calculator/src/main/java/student/grade/calculator/GradeCalculator.java
@@ -8,6 +8,12 @@ public class GradeCalculator {
     private double averageMarksPercentage;
     private char grade;
 
+    /**
+     * Constructs a GradeCalculator object for a specified number of subjects.
+     * Initializes the marks array, total marks, average marks percentage, and grade.
+     *
+     * @param numberOfSubjects the number of subjects to calculate grades for
+     */
     public GradeCalculator(int numberOfSubjects) {
         marks = new int[numberOfSubjects];
         totalMarks = 0;
@@ -15,38 +21,84 @@ public class GradeCalculator {
         grade = ' ';
     }
 
+    /**
+     * Returns the marks obtained in each subject.
+     *
+     * @return an array of marks
+     */
     public int[] getMarks() {
         return marks;
     }
 
+    /**
+     * Returns the total marks obtained.
+     *
+     * @return the total marks
+     */
     public int getTotalMarks() {
         return totalMarks;
     }
 
+    /**
+     * Returns the average marks percentage.
+     *
+     * @return the average marks percentage
+     */
     public double getAverageMarksPercentage() {
         return averageMarksPercentage;
     }
 
+    /**
+     * Returns the grade based on the average marks percentage.
+     *
+     * @return the grade
+     */
     public char getGrade() {
         return grade;
     }
 
+    /**
+     * Sets the marks for each subject.
+     *
+     * @param marks an array of marks to set
+     */
     public void setMarks(int[] marks) {
         this.marks = marks;
     }
 
+    /**
+     * Sets the total marks obtained.
+     *
+     * @param totalMarks the total marks to set
+     */
     public void setTotalMarks(int totalMarks) {
         this.totalMarks = totalMarks;
     }
 
+    /**
+     * Sets the average marks percentage.
+     *
+     * @param averageMarksPercentage the average marks percentage to set
+     */
     public void setAverageMarksPercentage(double averageMarksPercentage) {
         this.averageMarksPercentage = averageMarksPercentage;
     }
 
+    /**
+     * Sets the grade based on the average marks percentage.
+     *
+     * @param grade the grade to set
+     */
     public void setGrade(char grade) {
         this.grade = grade;
     }
 
+    /**
+     * Inputs the marks for each subject from the user.
+     * Prompts the user to enter marks for each subject and calculates the total marks.
+     *
+     * @param scanner the scanner to read user input
+     */
     public void inputMarks(Scanner scanner) {
         System.out.println("Enter marks obtained in each subject (out of 100): ");
         for (int i = 0; i < marks.length; i++) {
@@ -56,15 +108,25 @@ public class GradeCalculator {
         }
     }
 
+    /**
+     * Displays the total marks obtained.
+     */
     public void TotalMarks() {
         System.out.println("Total Marks obtained: " + totalMarks);
     }
 
+    /**
+     * Calculates and displays the average marks percentage.
+     */
     public void AverageMarksPercentage() {
         averageMarksPercentage = (double) totalMarks / marks.length;
         System.out.println("Average Marks Percentage: " + averageMarksPercentage);
     }
 
+    /**
+     * Calculates and displays the grade based on the average marks percentage.
+     * Sets the grade according to the specified grading criteria.
+     */
     public void calculateGrade() {
         if (averageMarksPercentage >= 90) {
             grade = 'A';
@@ -74,7 +136,9 @@ public class GradeCalculator {
             grade = 'C';
         } else if (averageMarksPercentage >= 50) {
             grade = 'D';
-        } else grade = 'F';
+        } else {
+            grade = 'F';
+        }
         System.out.println("Grade is: " + grade);
     }
 }
--- student_grade_calculator/src/main/java/student/grade/calculator/GradingApp.java
+++ student_grade_calculator/src/main/java/student/grade/calculator/GradingApp.java
@@ -3,6 +3,14 @@ package student.grade.calculator;
 import java.util.Scanner;
 
 public class GradingApp {
+    /**
+     * The main method to start the grade calculation application.
+     * Prompts the user to enter the number of subjects and initializes a GradeCalculator object.
+     * Inputs marks for each subject, calculates and displays the total marks, average marks percentage, and grade.
+     * Closes the scanner when finished.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
