7dc575dd05d88af5bf6898b0c4c7f36070334355
akhumalo023a <akhumalo023a@student.wethinkcode.co.za>
2024-05-19 19:24:23 +0200

    Added docstrings

--- number_game/src/main/java/numberGame/Main.java
+++ number_game/src/main/java/numberGame/Main.java
@@ -1,6 +1,13 @@
 package numberGame;
 
 public class Main {
+    /**
+     * The main method to start the number guessing game application.
+     * Initializes a NumberGame object with a range from 1 to 100 and a maximum of 10 attempts.
+     * Starts the game by calling the gamePlay method.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         NumberGame game = new NumberGame(1, 100, 10);
         game.gamePlay();
--- number_game/src/main/java/numberGame/NumberGame.java
+++ number_game/src/main/java/numberGame/NumberGame.java
@@ -12,6 +12,14 @@ public class NumberGame {
     private final Random random;
     private int randomNumber;
 
+    /**
+     * Constructs a NumberGame object with the specified range and maximum number of attempts.
+     * Initializes the score to 0, and sets up a Scanner and Random instance.
+     *
+     * @param minRange the minimum range of the number to guess
+     * @param maxRange the maximum range of the number to guess
+     * @param maxAttempts the maximum number of attempts allowed
+     */
     public NumberGame(int minRange, int maxRange, int maxAttempts) {
         this.minRange = minRange;
         this.maxRange = maxRange;
@@ -22,7 +30,16 @@ public class NumberGame {
         this.random = new Random();
     }
 
-    // for testing purposes only
+    /**
+     * Constructs a NumberGame object with the specified range, maximum number of attempts, and a Random instance.
+     * Initializes the score to 0, and sets up a Scanner instance.
+     * This constructor is intended for testing purposes to use a provided Random instance.
+     *
+     * @param minRange the minimum range of the number to guess
+     * @param maxRange the maximum range of the number to guess
+     * @param maxAttempts the maximum number of attempts allowed
+     * @param random the Random instance to use for generating random numbers
+     */
     public NumberGame(int minRange, int maxRange, int maxAttempts, Random random) {
         this.minRange = minRange;
         this.maxRange = maxRange;
@@ -33,11 +50,24 @@ public class NumberGame {
         this.random = random;
     }
 
-    // For Testing purposes only
+    /**
+     * Returns the random number generated for the current round.
+     * This method is intended for testing purposes only.
+     *
+     * @return the random number for the current round
+     */
     public int getRandomNumber() {
         return randomNumber;
     }
 
+    /**
+     * Plays a single round of the number guessing game.
+     * Generates a random number within the specified range and allows the user to guess the number.
+     * Provides feedback on the user's guesses and returns true if the user guesses correctly within the allowed attempts.
+     * Returns false if the user fails to guess the number within the allowed attempts.
+     *
+     * @return true if the user guesses the number correctly, false otherwise
+     */
     public boolean playRound() {
         randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
         int attempts = 0;
@@ -58,7 +88,9 @@ public class NumberGame {
                     return true;
                 } else if (userGuess < randomNumber) {
                     System.out.println("Too low. Try again.");
-                } else System.out.println("Too high. Try again.");
+                } else {
+                    System.out.println("Too high. Try again.");
+                }
             } catch (NumberFormatException e) {
                 System.out.println("You entered an invalid number.");
             }
@@ -68,6 +100,11 @@ public class NumberGame {
         return false;
     }
 
+    /**
+     * Starts the number guessing game, allowing the user to play multiple rounds.
+     * Displays a welcome message and loops until the user chooses not to play again.
+     * Updates and displays the user's score, and closes the scanner when the game ends.
+     */
     public void gamePlay() {
         System.out.println("Welcome to the number game!");
         boolean playAgain = true;
